928f2e4ddab76fe655edadd2eb8731f1ebf22f7f
Abisheak2744 <113177222+Abisheak2744@users.noreply.github.com>
2023-07-11 09:21:55 +0530

    Add files via upload

---
+++ pro1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class pro1 {
+  public static void main(String[] args) {
+    int N;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a Number:");
+    N = sc.nextInt();
+
+    if(N > 0){
+      System.out.println("It is Positive");
+    }
+    else if(N < 0){
+      System.out.println("It is Negative");
+    }
+    else{
+      System.out.println("Number is Zero");
+    }
+    }
+    }
